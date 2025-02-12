76db845146c25503da8e4cd3a70b6065cdf639c7
baynhamch <130159962+baynhamch@users.noreply.github.com>
2025-02-12 18:07:39 -0500

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -2,6 +2,12 @@
 The Sliding Window technique is a way to efficiently process problems that involve subarrays or substrings 
 by moving two pointers (left and right) dynamically instead of checking all possible substrings 
 (which would be slow).
+
+Used in this problem
+1.	A HashSet to store characters in the current substring.
+2.	Two pointers (l and r) to define a “window” in the string.
+3.	Expanding the window (r++) when there are no duplicates.
+4.	Shrinking the window (l++) when a duplicate is found.
 """
 
 
