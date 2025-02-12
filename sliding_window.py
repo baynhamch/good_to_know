362d6e29b4933450f33e5ec1586206b16cf9b755
baynhamch <130159962+baynhamch@users.noreply.github.com>
2025-02-12 17:59:36 -0500

    Create sliding_window.py

---
+++ sliding_window.py
@@ -0,0 +1,14 @@
+def lengthOfLongestSubstring(s):
+    char_set = set()  # HashSet to store unique characters
+    l = 0  # Left pointer
+    max_length = 0  # To store the max length of unique substring
+
+    for r in range(len(s)):  # Right pointer moves through s
+        while s[r] in char_set:  # If duplicate found
+            char_set.remove(s[l])  # Remove leftmost character
+            l += 1  # Move left pointer right
+        
+        char_set.add(s[r])  # Add the new character to the set
+        max_length = max(max_length, r - l + 1)  # Update max length
+
+    return max_length
